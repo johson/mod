0a748579d1a26f8c172e7542e3d5572eefcb1b4a
johson <php.johson@gmail.com>
2021-09-22 11:29:43 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module mod
+
+go 1.16
+
+require codeup.aliyun.com/vintop/common v1.0.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"codeup.aliyun.com/vintop/common/common/util"
+)
+
+func main() {
+	fmt.Println("vim-go")
+	fmt.Println(util.StrNow())
+}
